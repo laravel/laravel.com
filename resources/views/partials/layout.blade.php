6367d4d6c6ecb1be2af185e8bdfbc8b0602c30dc
Bill Riess <kryptyx@gmail.com>
2019-08-20 15:01:48 -0400

    Updated title tag to match new tagline.
    The landing page now states "Laravel - The framework for Creative Programmers" not "Web Artisans".

--- resources/views/partials/layout.blade.php
+++ resources/views/partials/layout.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>{{ isset($title) ? $title . ' - ' : null }}Laravel - The PHP Framework For Web Artisans</title>
+    <title>{{ isset($title) ? $title . ' - ' : null }}Laravel - The PHP Framework For Creative Programmers</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
     <!-- Favicon -->
