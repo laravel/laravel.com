36a1037f5e747d667b2514a824f5cc588173ad46
Ivan Dokov <ivan@jetspark.io>
2019-09-02 11:54:10 +0300

    Removed Mousetrap dependency as it is used only for a single key binding

--- resources/views/partials/layout.blade.php
+++ resources/views/partials/layout.blade.php
@@ -25,7 +25,6 @@
 
     <!-- Load JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mousetrap/1.4.6/mousetrap.min.js"></script>
 
 </head>
 <body class="language-php">
