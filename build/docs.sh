6ce099d9f5a95b7a3340898f8ad4d23bd4a5a39c
Taylor Otwell <taylor@laravel.com>
2019-09-13 10:42:27 -0500

    wip

--- build/docs.sh
+++ build/docs.sh
@@ -13,6 +13,7 @@ cd ${docs}/5.6 && git pull origin 5.6
 cd ${docs}/5.7 && git pull origin 5.7
 cd ${docs}/5.8 && git pull origin 5.8
 cd ${docs}/6.0 && git pull origin 6.0
+cd ${docs}/6.x && git pull origin 6.x
 cd ${docs}/master && git pull origin master
 
 cd $base && php artisan docs:clear-cache
