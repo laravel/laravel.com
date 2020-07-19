f891c70902db70fa29379e3a3e82b7f83231255a
William Desportes <williamdes@wdes.fr>
2020-07-19 14:22:52 +0200

    Rename Sami to Doctum

--- build/api.sh
+++ build/api.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 base=/home/forge/laravel.com
-sami=${base}/build/sami
+doctum=${base}/build/doctum
 
-cd $sami
+cd $doctum
 composer install
 
 # Cleanup Before
-rm -rf ${sami}/build
-rm -rf ${sami}/cache
-rm -rf ${sami}/laravel
+rm -rf ${doctum}/build
+rm -rf ${doctum}/cache
+rm -rf ${doctum}/laravel
 
 # Run API Docs
-git clone https://github.com/laravel/framework.git ${sami}/laravel
+git clone https://github.com/laravel/framework.git ${doctum}/laravel
 
-${sami}/vendor/bin/sami.php update ${sami}/sami.php
+${doctum}/vendor/bin/doctum.php update ${doctum}/doctum.php
 
 # Delete old directory before copying new one
 rm -rf ${base}/public/api
 
 # Copy new docs to public path
-cp -R ${sami}/build ${base}/public/api
+cp -R ${doctum}/build ${base}/public/api
 
 # Cleanup After
-rm -rf ${sami}/build
-rm -rf ${sami}/cache
-rm -rf ${sami}/laravel
+rm -rf ${doctum}/build
+rm -rf ${doctum}/cache
+rm -rf ${doctum}/laravel
