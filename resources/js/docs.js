1a082222034dc0fe6ee35c8e378c61dc98aea42b
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2020-08-12 09:04:47 +0600

    Add dark theme for documentation

--- resources/js/docs.js
+++ resources/js/docs.js
@@ -1,31 +1,53 @@
-require('./vendor/prism.js');
+require("./vendor/prism.js");
 
-$(document).ready(function () {
-    $('.navigation_contain').show();
+$(document).ready(function() {
+    $(".navigation_contain").show();
 });
 
-var current = $('.docs_sidebar ul').find('li a[href="' + window.location.pathname + '"]');
+var current = $(".docs_sidebar ul").find(
+    'li a[href="' + window.location.pathname + '"]'
+);
 
 if (current.length) {
-    current.parent().parent().parent().addClass('sub--on');
+    current
+        .parent()
+        .parent()
+        .parent()
+        .addClass("sub--on");
 
-    current.parent().addClass('active');
+    current.parent().addClass("active");
 }
 
-$(".docs_sidebar h2").click(function (e) {
+$(".docs_sidebar h2").click(function(e) {
     e.preventDefault();
-    let currenty_active = $(this).parent().hasClass('sub--on');
+    let currenty_active = $(this)
+        .parent()
+        .hasClass("sub--on");
     $(".navigation_contain ul li").removeClass("sub--on");
-    if(!currenty_active) {
-        $(this).parent().addClass("sub--on");
+    if (!currenty_active) {
+        $(this)
+            .parent()
+            .addClass("sub--on");
     }
 });
 
-$('#version_switcher').change(function(e){
-    window.location = $('#version_switcher').val();
+$("#version_switcher").change(function(e) {
+    window.location = $("#version_switcher").val();
 });
 
-$('.docs_main blockquote p').each(function () {
+$("#theme_switcher").change(function(e) {
+    const theme = e.target.value;
+    const path = "/docs";
+    document.cookie = "docs_theme=" + theme + "; path=" + path;
+    const body = $("body");
+    if (theme === "light") {
+        body.removeClass("dark");
+    } else {
+        body.addClass("dark");
+    }
+});
+
+$(".docs_main blockquote p").each(function() {
     var str = $(this).html();
     var match = str.match(/\{(.*?)\}/);
     var img, color;
@@ -37,24 +59,28 @@ $('.docs_main blockquote p').each(function () {
     if (type) {
         switch (type) {
             case "note":
-                img = '/img/callouts/exclamation.min.svg';
-                color = 'red';
+                img = "/img/callouts/exclamation.min.svg";
+                color = "red";
                 break;
             case "tip":
-                img = '/img/callouts/lightbulb.min.svg';
-                color = 'purple';
+                img = "/img/callouts/lightbulb.min.svg";
+                color = "purple";
                 break;
             case "laracasts":
             case "video":
-                img = '/img/callouts/laracast.min.svg';
-                color = 'blue';
+                img = "/img/callouts/laracast.min.svg";
+                color = "blue";
                 break;
         }
 
-        $(this).wrap('<div></div>');
-        $(this).before('<div class="icon ' + color + '"><img src="' + img + '"></div>');
-        $(this).html(str.replace(/\{(.*?)\}/, ''));
-        $(this).addClass('content');
-        $(this).parent().addClass('callout');
+        $(this).wrap("<div></div>");
+        $(this).before(
+            '<div class="icon ' + color + '"><img src="' + img + '"></div>'
+        );
+        $(this).html(str.replace(/\{(.*?)\}/, ""));
+        $(this).addClass("content");
+        $(this)
+            .parent()
+            .addClass("callout");
     }
 });
--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -1,77 +1,98 @@
 @extends('partials.layout')
 
 @section('content')
-    <div class="sidebar_layout" id="docsScreen">
+<div class="sidebar_layout" id="docsScreen">
 
-        <div class="page_contain">
-            <div class="contain">
-                <aside class="sidebar">
-                    <a href="/" class="logo">
-                        <img class="mark" src="/img/logomark.min.svg" alt="Laravel">
-                        <img class="type" src="/img/logotype.min.svg" alt="Laravel">
-                    </a>
-                    <nav>
-                        <div class="navigation_contain">
-                            <div class="docs_sidebar">
-                                {!! $index !!}
-                            </div>
-
-                            <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CKYILK3E&placement=laravelcom" id="_carbonads_js"></script>
-
-
+    <div class="page_contain">
+        <div class="contain">
+            <aside class="sidebar">
+                <a href="/" class="logo">
+                    <img class="mark" src="/img/logomark.min.svg" alt="Laravel">
+                    <img class="type" src="/img/logotype.min.svg" alt="Laravel">
+                </a>
+                <nav>
+                    <div class="navigation_contain">
+                        <div class="docs_sidebar">
+                            {!! $index !!}
                         </div>
-                    </nav>
-                    <div class="trigger_contain">
-                        <a href="#" class="nav_trigger" aria-label="Menu">
-                            <div class="bar"></div>
-                        </a>
+
+                        <script async type="text/javascript"
+                            src="//cdn.carbonads.com/carbon.js?serve=CKYILK3E&placement=laravelcom" id="_carbonads_js">
+                        </script>
+
+
                     </div>
-                </aside>
+                </nav>
+                <div class="trigger_contain">
+                    <a href="#" class="nav_trigger" aria-label="Menu">
+                        <div class="bar"></div>
+                    </a>
+                </div>
+            </aside>
 
-                <section class="body_content">
-                    <header class="docs_actions">
-                        <div class="version_drop">
-                            <div class="input_group">
-                                <label>Version</label>
-                                <div class="custom_select">
-                                    <select id="version_switcher" aria-label="Laravel version">
-                                        @foreach ($versions as $key => $display)
-                                            <option {{ $currentVersion == $key ? 'selected' : '' }} value="{{ url('docs/'.$key.$currentSection) }}">{{ $display }}</option>
-                                        @endforeach
-                                    </select>
+            <section class="body_content">
+                <header class="docs_actions">
+                    <div class="theme_mode">
+                        <div class="input-group">
+                            <label>Docs Theme</label>
+                            <div class="custom_select">
+                                <select id="theme_switcher" aria-label="Laravel docs theme">
+                                    <option value="light">Light</option>
+                                    <option value="dark"
+                                        {{ isset($_COOKIE['docs_theme']) && $_COOKIE['docs_theme'] == 'dark' ? 'selected' : '' }}>
+                                        Dark</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="version_drop">
+                        <div class="input_group">
+                            <label>Version</label>
+                            <div class="custom_select">
+                                <select id="version_switcher" aria-label="Laravel version">
+                                    @foreach ($versions as $key => $display)
+                                    <option {{ $currentVersion == $key ? 'selected' : '' }}
+                                        value="{{ url('docs/'.$key.$currentSection) }}">{{ $display }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="search_contain">
+                        <div class="input_group search icon">
+                            <input type="text" class="search_input" id="search-docs-input" placeholder="Search Docs"
+                                aria-label="Search in the documentation">
+                            <input type="submit" class="search_submit icon_display">
+                        </div>
+                    </div>
+
+                </header>
+                <section class="docs_body">
+                    <section class="docs_main">
+                        @unless (in_array($currentVersion, [DEFAULT_VERSION, 'master']))
+                        <blockquote>
+                            <div class="callout">
+                                <div class="icon orange">
+                                    <img src="{{ asset('/img/callouts/exclamation.min.svg') }}" alt="Icon" />
                                 </div>
-                            </div>
-                        </div>
-                        <div class="search_contain">
-                            <div class="input_group search icon">
-                                <input type="text" class="search_input" id="search-docs-input" placeholder="Search Docs" aria-label="Search in the documentation">
-                                <input type="submit" class="search_submit icon_display">
-                            </div>
-                        </div>
-                    </header>
-                    <section class="docs_body">
-                        <section class="docs_main">
-                            @unless (in_array($currentVersion, [DEFAULT_VERSION, 'master']))
-                                <blockquote>
-                                    <div class="callout">
-                                        <div class="icon orange">
-                                            <img src="{{ asset('/img/callouts/exclamation.min.svg') }}" alt="Icon"/>
-                                        </div>
 
-                                        <p class="content">
-                                            <strong>WARNING</strong> You're browsing the documentation for an old version of Laravel.
-                                            Consider upgrading your project to <a href="{{ route('docs.version', DEFAULT_VERSION) }}">Laravel {{ DEFAULT_VERSION }}</a>.
-                                        </p>
-                                    </div>
-                                </blockquote>
-                            @endunless
+                                <p class="content">
+                                    <strong>WARNING</strong> You're browsing the documentation for an old version of
+                                    Laravel.
+                                    Consider upgrading your project to <a
+                                        href="{{ route('docs.version', DEFAULT_VERSION) }}">Laravel
+                                        {{ DEFAULT_VERSION }}</a>.
+                                </p>
+                            </div>
+                        </blockquote>
+                        @endunless
 
-                            {!! $content !!}
-                        </section>
+                        {!! $content !!}
                     </section>
                 </section>
-            </div>
+            </section>
         </div>
-
     </div>
+
+</div>
 @stop
