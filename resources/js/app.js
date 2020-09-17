7e7db777c7ab8584898296883530436149f2f316
Abdelhammied Elsayed <ahe@crossworkers.com>
2020-09-17 10:40:29 +0200

    Adding New Feature Is Done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,23 @@
-$(document).ready(function () {
-    // Navigation Menu opener for small screens...
-    $(".nav_trigger").click(function (e) {
+$(document).ready(function() {
+    // Menu Opener
+    const navigateMenuOpener = (e) => {
         e.preventDefault();
         $("html").toggleClass("nav--on");
         $(".bar").toggleClass("animate");
+    }
+
+    // Navigation Menu opener for small screens...
+    $(".nav_trigger").click(navigateMenuOpener);
+
+    // Navigate Menu opener on esc click
+    $(document).keydown(function(e) {
+        var code = e.keyCode || e.which;
+
+        if (code == 27) {
+            navigateMenuOpener(e)
+        }
     });
+
 
     $(document).on('keydown', function(e) {
         var slashKey = e.key === '/' || e.which === 191 || e.keyCode === 191;
@@ -18,14 +31,14 @@ $(document).ready(function () {
 
 
     // Footer Navigation Menu opener for small screens...
-    $(".footer_nav_trigger").click(function (e) {
+    $(".footer_nav_trigger").click(function(e) {
         e.preventDefault();
         $(this).parent().toggleClass("nav--on");
     });
 
 
     // Main Menu Dropdown opener.
-    $(".drop_trigger").click(function (e) {
+    $(".drop_trigger").click(function(e) {
         e.preventDefault();
         $(this).parent().toggleClass("drop--on");
     });
@@ -41,20 +54,20 @@ $(document).ready(function () {
     }
 
     // Header Search Box...
-    $(".header_content .search_box").click(function (e) {
+    $(".header_content .search_box").click(function(e) {
         $(this).addClass("search--on");
     });
 
-    $(document).mouseup(function (e) {
+    $(document).mouseup(function(e) {
         var container = $(".header_content .search_box");
 
-        if (!container.is(e.target)
-            && container.has(e.target).length === 0) {
+        if (!container.is(e.target) &&
+            container.has(e.target).length === 0) {
             container.removeClass("search--on");
         }
     });
 
-    $('.docs_body').find('a[name]').each(function () {
+    $('.docs_body').find('a[name]').each(function() {
         var anchor = $('<a href="#' + this.name + '"/>');
         $(this).parent().next().wrapInner(anchor);
     });
