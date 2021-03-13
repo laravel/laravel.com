61244d3812d9ccd9cc7fd4c320c1ca007a411e0e
Priyash Patil <38959321+priyashpatil@users.noreply.github.com>
2021-03-13 18:01:24 +0530

    Switch cloning docs to http - replace ssh clone with http - wrap $v var with double quote to prevent globbing and word splitting

--- bin/checkout_latest_docs.sh
+++ bin/checkout_latest_docs.sh
@@ -20,9 +20,9 @@ DOCS_VERSIONS=(
 for v in "${DOCS_VERSIONS[@]}"; do
     if [ -d "resources/docs/$v" ]; then
         echo "Pulling latest documentation updates for $v..."
-        (cd resources/docs/$v && git pull)
+        (cd resources/docs/"$v" && git pull)
     else
         echo "Cloning $v..."
-        git clone --single-branch --branch "$v" git@github.com:laravel/docs.git "resources/docs/$v"
+        git clone --single-branch --branch "$v" https://github.com/laravel/docs.git "resources/docs/$v"
     fi;
 done
