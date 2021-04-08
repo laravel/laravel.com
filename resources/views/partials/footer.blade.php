fbcd59dbfb61bff4f92369ad11632701d75a7145
Taylor Otwell <taylorotwell@gmail.com>
2021-04-08 13:46:09 -0500

    wip

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -39,6 +39,7 @@
                 'A2 Design' => 'https://www.a2design.biz/',
                 'ABOUT YOU' => 'https://corporate.aboutyou.de/app/uploads/2019/08/INTRO-Pitch-I-AY-Tech.pdf?utm_source=laravelpartnersfindoutmore&utm_medium=socialgroups&utm_campaign=tech',
                 'Cubet' => 'https://cubettech.com/',
+                'Curotec' => 'https://www.curotec.com/services/technologies/laravel/',
                 'Cyber-Duck' => 'https://www.cyber-duck.co.uk/how-we-work/technology/laravel?utm_source=Laravel%20Partner&utm_medium=Sponsorship',
                 'DevSquad' => 'https://devsquad.com/',
                 'Ideil' => 'https://www.ideil.com/',
