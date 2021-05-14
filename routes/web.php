2bf600863d0d4034db6ae44ca85392607d9092af
Taylor Otwell <taylorotwell@gmail.com>
2021-05-14 12:09:12 -0500

    wip

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,14 @@ Route::get('partners', function () {
     return redirect('https://partners.laravel.com');
 });
 
+Route::get('/partner/ideil', function () {
+    return redirect('https://partners.laravel.com/partners/ideil', 301);
+});
+
+Route::get('/partner/curotec', function () {
+    return redirect('https://partners.laravel.com/partners/curotec', 301);
+});
+
 // Route::get('partner/{partner}', 'PartnersController@show');
 
 Route::get('/', function () {
