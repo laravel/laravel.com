a4ec05b659cbed0df7b273f5dd3f50ab2364ed1f
Taylor Otwell <taylorotwell@gmail.com>
2021-05-20 16:46:09 -0500

    wip

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -29,6 +29,26 @@
             ],
         ],
         [
+             'title' => 'Partners',
+             'links' => [
+                 'Vehikl' => 'https://vehikl.com',
+                 'Tighten' => 'https://tighten.co',
+                 '64 Robots' => 'https://64robots.com/',
+                 'Kirschbaum' => 'https://kirschbaumdevelopment.com/',
+                 'Curotec' => 'https://www.curotec.com/services/technologies/laravel/',
+                 'Jump24' => 'https://jump24.co.uk/',
+                 'A2 Design' => 'https://www.a2design.biz/',
+                 'ABOUT YOU' => 'https://corporate.aboutyou.de/app/uploads/2019/08/INTRO-Pitch-I-AY-Tech.pdf?utm_source=laravelpartnersfindoutmore&utm_medium=socialgroups&utm_campaign=tech',
+                 'Byte 5' => 'https://www.byte5.net/',
+                 'Cubet' => 'https://cubettech.com/',
+                 'Cyber-Duck' => 'https://www.cyber-duck.co.uk/how-we-work/technology/laravel?utm_source=Laravel%20Partner&utm_medium=Sponsorship',
+                 'DevSquad' => 'https://devsquad.com/',
+                 'Ideil' => 'https://www.ideil.com/',
+                 'Romega Software' => 'https://romegadigital.com/',
+                 'Worksome' => 'https://www.worksome.com/',
+             ]
+         ],
+        [
             'title' => 'Ecosystem',
             'links' => [
                 'Vapor' => 'https://vapor.laravel.com',
