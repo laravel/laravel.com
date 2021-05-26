28a1f90830b112a4b73da48b5fc7a18f284d9017
Taylor Otwell <taylorotwell@gmail.com>
2021-05-26 10:56:52 -0500

    wip

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -7,7 +7,7 @@
         <div class="content_contain">
             <div class="relative overflow-hidden">
                 <div class="contain">
-                    <section class="py-12 md:py-20 xl:py-28">
+                    <section class="pt-12 md:pt-20 xl:pt-20">
                         <div style="width: 120%;z-index: -9998" class="hidden md:transform md:translate-x-1/4 md:absolute md:-top-24 md:-right-48 md:flex md:items-center lg:-top-32 xl:-top-48">
                             <video poster="/img/blocks/blocks_1.jpg" playsinline autoplay muted loop>
                                 <source src="/img/blocks/blocks_1.mp4" type="video/mp4">
@@ -16,13 +16,13 @@
                         <div class="max-w-screen-xl mx-auto px-8 space-y-8 md:space-y-0 md:flex md:items-end">
                             <div class="md:flex-1">
                                 <h1 class="max-w-md font-medium text-3xl tracking-tight sm:text-4xl md:max-w-4xl md:text-5xl md:leading-tight xl:text-6xl">The Laravel Team</h1>
-                                <p class="mt-3 max-w-xl text-gray-600 sm:mt-5 md:max-w-2xl md:mt-8 md:text-lg">Laravel is a team of developers from all around the world.</p>
+                                <p class="mt-3 max-w-xl text-gray-600 sm:mt-5 md:max-w-2xl md:mt-8 md:text-lg">Laravel is a team of passionate developers from all around the world. We love building tools that make your life as a developer enjoyable, fun, and fulfilling. Follow us on Twitter and GitHub to keep up with what we're working on!</p>
                             </div>
                         </div>
                     </section>
                 </div>
 
-                <div class="max-w-screen-xl mx-auto px-8 py-12 md:py-32">
+                <div class="max-w-screen-xl mx-auto px-8 py-12">
                     <ul class="grid grid-cols-1 gap-x-4 gap-y-6 md:grid-cols-2 lg:grid-cols-4 lg:gap-8">
                         @foreach($team as $staff)
                         <li>
@@ -44,9 +44,10 @@
                                     <li>
                                         <a href="https://twitter.com/{{ $staff['twitter_username'] }}" target="_blank" class="text-gray-400 hover:text-gray-500">
                                             <span class="sr-only">Twitter</span>
-                                                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
-                                                    <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                                                </svg>
+
+                                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                                                <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                                            </svg>
                                         </a>
                                     </li>
                                     @endif
