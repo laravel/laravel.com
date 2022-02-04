84b967d02d1e7aa75b27318eccbcb7a925623386
Jason Beggs <jason@roasted.dev>
2022-02-04 16:07:07 -0500

    Add attribute commonmark extension

--- app/Markdown/GithubFlavoredMarkdownConverter.php
+++ app/Markdown/GithubFlavoredMarkdownConverter.php
@@ -7,6 +7,7 @@ use League\CommonMark\Environment\Environment;
 use App\Markdown\GithubFlavoredMarkdownExtension;
 use Torchlight\Commonmark\V2\TorchlightExtension;
 use League\CommonMark\Environment\EnvironmentInterface;
+use League\CommonMark\Extension\Attributes\AttributesExtension;
 use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
 
 /**
@@ -24,6 +25,7 @@ class GithubFlavoredMarkdownConverter extends MarkdownConverter
         $environment = new Environment($config);
         $environment->addExtension(new CommonMarkCoreExtension());
         $environment->addExtension(new GithubFlavoredMarkdownExtension());
+        $environment->addExtension(new AttributesExtension());
         $environment->addExtension(new TorchlightExtension());
 
         parent::__construct($environment);
