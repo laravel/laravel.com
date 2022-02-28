397cbb5dd20a5368574f0cba135d8ad48714dc31
Ihor Sychevskyi <arhell333@gmail.com>
2022-02-28 05:19:54 +0200

    Accessibility add title to btn (#222)
    * Accessibility add title to btn
    
    * Update header.blade.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -274,7 +274,7 @@
                 <li><a href="https://partners.laravel.com">Partners</a></li>
             </ul>
             <div class="flex-1 flex items-center justify-end">
-                <button @click.prevent="$dispatch('toggle-search-modal')">
+                <button @click.prevent="$dispatch('toggle-search-modal')" title="Search">
                     <svg class="w-6 h-6 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                 </button>
                 <x-button.secondary href="/docs" class="hidden lg:ml-4 lg:inline-flex">Documentation</x-button.secondary>
