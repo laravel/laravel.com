1a2ec4cdbb75292cfc118bd799398d0680469cbb
Džuris <juris@glaive.pro>
2022-06-29 06:18:34 +0300

    Fix alpine typo in search modal

--- resources/views/components/search-modal.blade.php
+++ resources/views/components/search-modal.blade.php
@@ -31,7 +31,7 @@
             >
                 <svg class="absolute inset-y-0 left-0 mt-1 w-5 h-5 text-gray-500 pointer-events-none" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                 <input
-                    x-model.debouce.200ms="search"
+                    x-model.debounce.200ms="search"
                     x-ref="searchInput"
                     class="flex-1 w-full pl-8 pr-4 py-1 tracking-wide text-gray-400 placeholder-gray-500 bg-transparent focus:outline-none"
                     placeholder="Search Docs (Press '/')"
