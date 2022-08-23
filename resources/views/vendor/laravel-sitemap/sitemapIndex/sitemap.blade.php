8883513fcfbe1464ca8317f19a16a6e579bad567
James Brooks <james@alt-three.com>
2022-08-23 10:24:44 +0100

    Remove published vendor files

--- resources/views/vendor/laravel-sitemap/sitemapIndex/sitemap.blade.php
+++
@@ -1,9 +0,0 @@
-<sitemap>
-    @if (! empty($tag->url))
-    <loc>{{ url($tag->url) }}</loc>
-    @endif
-
-    @if (! empty($tag->lastModificationDate))
-    <lastmod>{{ $tag->lastModificationDate->format(DateTime::ATOM) }}</lastmod>
-    @endif
-</sitemap>
