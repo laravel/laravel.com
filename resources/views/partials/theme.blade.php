a950dfc1f5712556e1c931e3ad1aff928ebce97f
Dries Vints <dries@vints.io>
2023-04-02 18:21:07 +0200

    Dark mode for search (#281)
    * Dark mode for search
    
    * wip
    
    * Compile Assets
    
    ---------
    
    Co-authored-by: driesvints <driesvints@users.noreply.github.com>

--- resources/views/partials/theme.blade.php
+++ resources/views/partials/theme.blade.php
@@ -3,8 +3,10 @@
         if (localStorage.theme === 'system') {
             if (e.matches) {
                 document.documentElement.classList.add('dark');
+                document.documentElement.setAttribute('data-theme', 'dark');
             } else {
                 document.documentElement.classList.remove('dark');
+                document.documentElement.setAttribute('data-theme', 'light');
             }
         }
 
@@ -20,20 +22,25 @@
             case 'system':
                 if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
                     document.documentElement.classList.add('dark');
+                    document.documentElement.setAttribute('data-theme', 'dark');
                 } else {
                     document.documentElement.classList.remove('dark');
+                    document.documentElement.setAttribute('data-theme', 'light');
                 }
+
                 document.documentElement.setAttribute('color-theme', 'system');
                 break;
 
             case 'dark':
                 document.documentElement.classList.add('dark');
                 document.documentElement.setAttribute('color-theme', 'dark');
+                document.documentElement.setAttribute('data-theme', 'dark');
                 break;
 
             case 'light':
                 document.documentElement.classList.remove('dark');
                 document.documentElement.setAttribute('color-theme', 'light');
+                document.documentElement.setAttribute('data-theme', 'light');
                 break;
         }
 
