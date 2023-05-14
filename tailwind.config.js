18956a107bef166f9611ba5ad6aa44e96c539839
Ngo Quoc Dat <datlechin@gmail.com>
2023-05-14 21:36:53 +0700

    Migrate to module

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+import defaultTheme from 'tailwindcss/defaultTheme';
 
-module.exports = {
+export default {
   content: [
       'resources/views/**/*.blade.php',
       'resources/js/**/*.js',
