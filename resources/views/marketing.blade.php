7d5954a2102a8e7fa40b9d9d873d61613d44d51d
Jess Archer <jess@jessarcher.com>
2023-05-24 10:57:20 +1000

    Use new features section on the home page

--- resources/views/marketing.blade.php
+++ resources/views/marketing.blade.php
@@ -94,30 +94,23 @@
 
     <div class="relative overflow-hidden py-16 md:pt-48">
         <x-radial-blur class="right-[-20%] top-0 w-[640px] h-[640px]" />
-        <div class="max-w-screen-xl w-full mx-auto px-5">
-            <h2 class="text-4xl font-bold max-w-lg md:text-5xl">Everything you need to be amazing.</h2>
-            <div class="mt-14 grid gap-10 sm:grid-cols-2 lg:grid-cols-4">
-                <div>
-                    <svg class="w-8 h-8 text-red-500" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path stroke="#F9322C" d="M.5.5h31v31H.5z"/><path d="M8 11v10c0 2.21 3.582 4 8 4s8-1.79 8-4V11M8 11c0 2.21 3.582 4 8 4s8-1.79 8-4M8 11c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" stroke="#F9322C" stroke-linecap="round" stroke-linejoin="round"/></svg>
-                    <h3 class="mt-5 text-xl font-bold">Database</h3>
-                    <p class="mt-4 text-gray-700 text-sm leading-relaxed">Laravel's <a class="underline" href="/docs/eloquent">"Eloquent" ORM</a> provides best-in-class database abstraction without the headaches. Query and update your data without breaking a sweat. Eloquent pairs perfectly with MySQL, Postgres, SQLite, and SQL Server.</p>
-                </div>
-                <div>
-                    <svg class="w-8 h-8 text-red-500" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path stroke="currentColor" d="M.5.5h31v31H.5z"/><path d="M23 15H9m14 0a2 2 0 0 1 2 2v6a2 2 0 0 1-2 2H9a2 2 0 0 1-2-2v-6a2 2 0 0 1 2-2m14 0v-2a2 2 0 0 0-2-2M9 15v-2a2 2 0 0 1 2-2m10 0V9a2 2 0 0 0-2-2h-6a2 2 0 0 0-2 2v2m10 0H11" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/></svg>
-                    <h3 class="mt-5 text-xl font-bold">Queues</h3>
-                    <p class="mt-4 text-gray-700 text-sm leading-relaxed">Dispatch background jobs to perform slow tasks like sending emails and generating reports while maintaining blazing fast response times. Laravel's <a class="underline" href="/docs/queues">robust queue system</a> can process jobs using Redis, Amazon SQS, or even MySQL and Postgres.</p>
-                </div>
-                <div>
-                    <svg class="w-8 h-8 text-red-500" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path stroke="#F9322C" d="M.5.5h31v31H.5z"/><path d="m10.171 15.828-2.12 2.122M19 19l-2 5-4-11 11 4-5 2Zm0 0 5 5-5-5ZM11.188 6.239l.777 2.897-.777-2.897Zm-2.052 5.726-2.898-.777 2.898.777ZM17.95 8.05l-2.122 2.122L17.95 8.05Z" stroke="#F9322C" stroke-linecap="round" stroke-linejoin="round"/></svg>
-                    <h3 class="mt-5 text-xl font-bold">WebSockets</h3>
-                    <p class="mt-4 text-gray-700 text-sm leading-relaxed"><a class="underline" href="/docs/broadcasting">Laravel Echo</a> and event broadcasting make it a cinch to build modern, realtime user experiences. Create amazing realtime applications while powering your WebSockets with pure PHP, Node.js, or serverless solutions like <a class="underline" href="https://pusher.com">Pusher</a> and <a class="underline" href="https://ably.com">Ably</a>.</p>
-                </div>
-                <div>
-                    <svg class="w-8 h-8 text-red-500" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path stroke="#F9322C" d="M.5.5h31v31H.5z"/><path d="M19 11a2 2 0 0 1 2 2l-2-2Zm6 2a6 6 0 0 1-7.743 5.743L15 21h-2v2h-2v2H8a1 1 0 0 1-1-1v-2.586a1 1 0 0 1 .293-.707l5.964-5.964A6 6 0 1 1 25 13v0Z" stroke="#F9322C" stroke-linecap="round" stroke-linejoin="round"/></svg>
-                    <h3 class="mt-5 text-xl font-bold">Authentication</h3>
-                    <p class="mt-4 text-gray-700 text-sm leading-relaxed">Stop sweating authentication. Laravel provides scaffolding for secure, session based authentication, while <a class="underline" href="/docs/sanctum">Laravel Sanctum</a> provides painless authentication for APIs and mobile applications.</p>
-                </div>
+        <div class="relative max-w-screen-xl w-full mx-auto px-5">
+            <div class="hidden lg:block z-0 absolute x-translate-y-12 top-[50px] pointer-events-none right-[300px]">
+                <x-cube />
             </div>
+            <div class="hidden lg:block z-0 absolute x-translate-y-20 pointer-events-none -right-[40px]">
+                <x-cube :delay="2000" />
+            </div>
+            <div class="hidden lg:block z-0 absolute top-[300px] x-translate-y-20 pointer-events-none right-[80px]">
+                <x-cube :delay="1000" />
+            </div>
+
+            <h2 class="text-4xl font-bold max-w-lg md:text-5xl">Everything you need to be amazing.</h2>
+            <p class="mt-6 text-gray-700 leading-relaxed max-w-3xl">Out of the box, Laravel has elegant solutions for the common features needed by all modern web applications. It's time to start building amazing applications and stop wasting time searching for packages and reinventing the wheel.</p>
+
+            @include('partials.features')
+
+            <p class="mt-12 text-gray-700 leading-relaxed max-w-3xl">We've just scratched the surface. Laravel has solutions for nearly everything you'll need when building a web application, such as email verification, rate limiting, and custom console commands. Check out the <a href="https://laravel.com/docs" class="text-red-600 underline hover:no-underline">Laravel documentation</a> to dig in further.</p>
         </div>
     </div>
 
