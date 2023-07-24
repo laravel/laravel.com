50fa2003fd4d0bf0a31ee88c09bff5230bd8bcf3
James Brooks <james@alt-three.com>
2023-07-24 15:45:51 +0100

    Add Shop links (#295)

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -28,6 +28,7 @@
                 'Laracon India' => 'https://laracon.in/',
                 'Jobs' => 'https://larajobs.com',
                 'Forums' => 'https://laracasts.com/discuss',
+                'Shop' => 'https://laravel.bigcartel.com/',
                 'Trademark' => '/trademark',
             ],
         ],
