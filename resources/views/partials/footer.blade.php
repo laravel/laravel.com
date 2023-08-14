587137c53f9e6473828d90b5207ab6a3c02b72d0
TheBlckbird <66938161+TheBlckbird@users.noreply.github.com>
2023-08-14 17:25:52 +0200

    Add missing Ecosystem items (#303)
    * Add Livewire link
    
    * Add Inertia link
    
    * Add footer links
    
    * Add description for Inertia
    
    * Add description for Livewire
    
    * Add laravel prompts
    
    * Fix sorting
    
    * Improve alt texts to Ecosystem icons
    
    * Add Inertia, Livewire & Prompts to Marketing Page
    
    * Refactor ecosystem items (DRY them)
    
    * Change id field to key
    
    * formatting
    
    * compile
    
    * formatting
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -34,44 +34,33 @@
             ],
         ],
         [
-             'title' => 'Partners',
-             'links' => [
-                 'WebReinvent' => 'https://webreinvent.com/?utm_source=laravel&utm_medium=laravel.com&utm_campaign=footer-link',
-                 'Vehikl' => 'https://vehikl.com',
-                 'Tighten' => 'https://tighten.co',
-                 '64 Robots' => 'https://64robots.com/',
-                 'Active Logic' => 'https://activelogic.com/',
-                 'Byte 5' => 'https://www.byte5.net/',
-                 'Curotec' => 'https://www.curotec.com/services/technologies/laravel/',
-                 'Cyber-Duck' => 'https://www.cyber-duck.co.uk/how-we-work/technology/laravel?utm_source=Laravel%20Partner&utm_medium=Sponsorship',
-                 'DevSquad' => 'https://devsquad.com/',
-                 'Jump24' => 'https://jump24.co.uk/',
-                 'Kirschbaum' => 'https://kirschbaumdevelopment.com/',
-             ]
-         ],
+            'title' => 'Partners',
+            'links' => [
+                'WebReinvent' => 'https://webreinvent.com/?utm_source=laravel&utm_medium=laravel.com&utm_campaign=footer-link',
+                'Vehikl' => 'https://vehikl.com',
+                'Tighten' => 'https://tighten.co',
+                '64 Robots' => 'https://64robots.com/',
+                'Active Logic' => 'https://activelogic.com/',
+                'Byte 5' => 'https://www.byte5.net/',
+                'Curotec' => 'https://www.curotec.com/services/technologies/laravel/',
+                'Cyber-Duck' => 'https://www.cyber-duck.co.uk/how-we-work/technology/laravel?utm_source=Laravel%20Partner&utm_medium=Sponsorship',
+                'DevSquad' => 'https://devsquad.com/',
+                'Jump24' => 'https://jump24.co.uk/',
+                'Kirschbaum' => 'https://kirschbaumdevelopment.com/',
+            ],
+        ],
         [
             'title' => 'Ecosystem',
             'links' => [
-                'Cashier' => '/docs/' . DEFAULT_VERSION . '/billing',
-                'Dusk' => '/docs/' . DEFAULT_VERSION . '/dusk',
-                'Echo' => '/docs/' . DEFAULT_VERSION . '/broadcasting',
-                'Envoyer' => 'https://envoyer.io',
-                'Forge' => 'https://forge.laravel.com',
-                'Herd' => 'https://herd.laravel.com',
-                'Horizon' => '/docs/' . DEFAULT_VERSION . '/horizon',
-                'Nova' => 'https://nova.laravel.com',
-                'Octane' => '/docs/' . DEFAULT_VERSION . '/octane',
-                'Pennant' => '/docs/' . DEFAULT_VERSION . '/pennant',
-                'Sail' => '/docs/' . DEFAULT_VERSION . '/sail',
-                'Sanctum' => '/docs/' . DEFAULT_VERSION . '/sanctum',
-                'Scout' => '/docs/' . DEFAULT_VERSION . '/scout',
-                'Spark' => 'https://spark.laravel.com',
-                'Telescope' => '/docs/' . DEFAULT_VERSION . '/telescope',
-                'Vapor' => 'https://vapor.laravel.com',
+                // See down below
             ],
         ],
     ];
-
+    
+    foreach (App\Ecosystem::items() as $ecosystemItem) {
+        $links[3]['links'][$ecosystemItem['name']] = $ecosystemItem['href'];
+    }
+    
     $is_docs_page = request()->is('docs/*');
 @endphp
 
@@ -85,7 +74,10 @@
 
         <div class="mt-6 grid grid-cols-12 md:gap-x-8 gap-y-12 sm:mt-12">
             <div class="col-span-12 lg:col-span-4">
-                <p class="max-w-sm text-xs text-gray-700 sm:text-sm {{ $is_docs_page ? 'dark:text-gray-500' : '' }}">Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel attempts to take the pain out of development by easing common tasks used in most web projects.</p>
+                <p class="max-w-sm text-xs text-gray-700 sm:text-sm {{ $is_docs_page ? 'dark:text-gray-500' : '' }}">
+                    Laravel is a web application framework with expressive, elegant syntax. We believe development must
+                    be an enjoyable and creative experience to be truly fulfilling. Laravel attempts to take the pain
+                    out of development by easing common tasks used in most web projects.</p>
                 <ul class="mt-6 flex items-center space-x-3">
                     <li>
                         <a href="https://x.com/laravelphp">
@@ -95,32 +87,46 @@
                     </li>
                     <li>
                         <a href="https://github.com/laravel">
-                            <img class="{{ $is_docs_page ? 'hidden dark:inline-block' : 'hidden' }} w-6 h-6" src="/img/social/github.dark.min.svg" alt="GitHub" width="24" height="24" loading="lazy">
-                            <img class="{{ $is_docs_page ? 'inline-block dark:hidden' : 'inline-block' }} w-6 h-6" src="/img/social/github.min.svg" alt="GitHub" width="24" height="24" loading="lazy">
+                            <img class="{{ $is_docs_page ? 'hidden dark:inline-block' : 'hidden' }} w-6 h-6"
+                                src="/img/social/github.dark.min.svg" alt="GitHub" width="24" height="24"
+                                loading="lazy">
+                            <img class="{{ $is_docs_page ? 'inline-block dark:hidden' : 'inline-block' }} w-6 h-6"
+                                src="/img/social/github.min.svg" alt="GitHub" width="24" height="24"
+                                loading="lazy">
                         </a>
                     </li>
                     <li>
                         <a href="https://discord.gg/mPZNm7A">
-                            <img class="{{ $is_docs_page ? 'hidden dark:inline-block' : 'hidden' }} w-6 h-6" src="/img/social/discord.dark.min.svg" alt="Discord" width="21" height="24" loading="lazy">
-                            <img class="{{ $is_docs_page ? 'inline-block dark:hidden' : 'inline-block' }} w-6 h-6" src="/img/social/discord.min.svg" alt="Discord" width="21" height="24" loading="lazy">
+                            <img class="{{ $is_docs_page ? 'hidden dark:inline-block' : 'hidden' }} w-6 h-6"
+                                src="/img/social/discord.dark.min.svg" alt="Discord" width="21" height="24"
+                                loading="lazy">
+                            <img class="{{ $is_docs_page ? 'inline-block dark:hidden' : 'inline-block' }} w-6 h-6"
+                                src="/img/social/discord.min.svg" alt="Discord" width="21" height="24"
+                                loading="lazy">
                         </a>
                     </li>
                     <li>
                         <a href="https://www.youtube.com/laravelphp">
-                            <img class="{{ $is_docs_page ? 'hidden dark:inline-block' : 'hidden' }} w-6 h-6" src="/img/social/youtube.dark.min.svg" alt="YouTube" width="169" height="150" loading="lazy">
-                            <img class="{{ $is_docs_page ? 'inline-block dark:hidden' : 'inline-block' }} w-6 h-6" src="/img/social/youtube.min.svg" alt="YouTube" width="169" height="150" loading="lazy">
+                            <img class="{{ $is_docs_page ? 'hidden dark:inline-block' : 'hidden' }} w-6 h-6"
+                                src="/img/social/youtube.dark.min.svg" alt="YouTube" width="169" height="150"
+                                loading="lazy">
+                            <img class="{{ $is_docs_page ? 'inline-block dark:hidden' : 'inline-block' }} w-6 h-6"
+                                src="/img/social/youtube.min.svg" alt="YouTube" width="169" height="150"
+                                loading="lazy">
                         </a>
                     </li>
                 </ul>
             </div>
             @foreach ($links as $column)
                 <div class="text-xs col-span-6 md:col-span-3 lg:col-span-2">
-                    <span class="uppercase {{ $is_docs_page ? 'dark:text-gray-200' : '' }}">{{ $column['title'] }}</span>
+                    <span
+                        class="uppercase {{ $is_docs_page ? 'dark:text-gray-200' : '' }}">{{ $column['title'] }}</span>
                     <div class="mt-5">
                         <ul class="space-y-3 text-gray-700 {{ $is_docs_page ? 'dark:text-gray-500' : '' }}">
                             @foreach ($column['links'] as $title => $href)
                                 <li>
-                                    <a href="{{ $href }}" class="transition-colors hover:text-gray-600 {{ $is_docs_page ? 'dark:hover:text-gray-400' : '' }}">{{ $title }}</a>
+                                    <a href="{{ $href }}"
+                                        class="transition-colors hover:text-gray-600 {{ $is_docs_page ? 'dark:hover:text-gray-400' : '' }}">{{ $title }}</a>
                                 </li>
                             @endforeach
                         </ul>
