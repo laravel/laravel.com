285091060cf5de95a0d721ba31c500a2a9a10531
Taylor Otwell <taylor@laravel.com>
2023-10-30 14:51:39 -0500

    wip

--- resources/views/components/header-news-bar.blade.php
+++ resources/views/components/header-news-bar.blade.php
@@ -37,7 +37,7 @@
         <div><svg class="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg></div>
 
         <div class="mt-px ml-1">
-            Servers with PHP 8.2 are now available for provisioning via <a href="https://forge.laravel.com" class="underline">Laravel Forge</a>.
+            Servers with PHP 8.3 are now available for provisioning via <a href="https://forge.laravel.com" class="underline">Laravel Forge</a>.
         </div>
 
     @elseif ($news === 'vapor')
