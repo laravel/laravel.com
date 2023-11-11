ace0b7fb4ed03957ebd1bc0afc8cce6a8152974b
514sid <140138716+514sid@users.noreply.github.com>
2023-11-11 22:44:34 +0400

    add docs config

---
+++ config/docs.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+	/*
+    |--------------------------------------------------------------------------
+    | Latest Laravel Version
+    |--------------------------------------------------------------------------
+    |
+    | This variable represents the latest Laravel version used in links to
+	| documentation and redirects. 
+    |
+    */
+
+	'latest_laravel_version' => env('LATEST_LARAVEL_VERSION', "10.x")
+
+];
