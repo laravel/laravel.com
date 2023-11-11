46e4a50a1edd831985e81c1e83d2b4d3b6992691
514sid <140138716+514sid@users.noreply.github.com>
2023-11-11 22:53:02 +0400

    share Laravel latest version with all views

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Documentation;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::share('latestVersion', Documentation::getLatestLaravelVersion());
     }
 }
