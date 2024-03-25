22c5f34313f58cdf3e87d653c56de949831a9232
Taylor Otwell <taylor@laravel.com>
2024-03-25 08:47:08 -0500

    wip

--- resources/views/components/header-news-bar.blade.php
+++ resources/views/components/header-news-bar.blade.php
@@ -1,5 +1,5 @@
 <?php
-    $news = Illuminate\Support\Arr::random(['nova', 'forge', 'vapor', 'pulse', 'laracon-in']);
+    $news = Illuminate\Support\Arr::random(['nova', 'forge', 'vapor', 'pulse', 'reverb']);
 ?>
 
 <div class="hidden lg:flex items-center justify-center bg-gradient-to-b from-red-500 to-red-600 p-2 text-center text-white text-sm">
@@ -52,6 +52,23 @@
 
         <div class="mt-px ml-1">
             How's your health? Check your application's vital signs using <a href="https://pulse.laravel.com" class="underline">Laravel Pulse</a>.
+        </div>
+
+    @elseif ($news === 'reverb')
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-white" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <path d="M3.707 6.293l2.586 -2.586a1 1 0 0 1 1.414 0l5.586 5.586a1 1 0 0 1 0 1.414l-2.586 2.586a1 1 0 0 1 -1.414 0l-5.586 -5.586a1 1 0 0 1 0 -1.414z" />
+              <path d="M6 10l-3 3l3 3l3 -3" />
+              <path d="M10 6l3 -3l3 3l-3 3" />
+              <path d="M12 12l1.5 1.5" />
+              <path d="M14.5 17a2.5 2.5 0 0 0 2.5 -2.5" />
+              <path d="M15 21a6 6 0 0 0 6 -6" />
+            </svg>
+        </div>
+
+        <div class="mt-px ml-1">
+            Incoming transmission received. <a href="https://pulse.laravel.com" class="underline">Laravel Reverb</a> is now available!
         </div>
 
     @elseif ($news === 'nova')
