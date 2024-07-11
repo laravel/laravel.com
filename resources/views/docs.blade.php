adda0756c4e6a8c04d420c06d24a7a49df952bc8
Pascal Bürkle <25956751+pabueco@users.noreply.github.com>
2024-07-11 18:31:38 +0200

    docs navigation: fix sticky position and separate scroll area

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -5,9 +5,9 @@
 
     <x-header-news-bar />
 
-    <div class="relative overflow-auto dark:bg-dark-700" id="docsScreen">
+    <div class="relative dark:bg-dark-700" id="docsScreen">
         <div class="relative lg:flex lg:items-start">
-            <aside class="hidden fixed top-0 bottom-0 left-0 z-20 h-full w-16 bg-gradient-to-b from-gray-100 to-white transition-all duration-300 overflow-hidden lg:sticky lg:w-80 lg:shrink-0 lg:flex lg:flex-col lg:justify-end lg:items-end 2xl:max-w-lg 2xl:w-full dark:from-dark-800 dark:to-dark-700">
+            <aside class="hidden fixed top-0 bottom-0 left-0 z-20 h-screen w-16 bg-gradient-to-b from-gray-100 to-white transition-all duration-300 overflow-auto lg:sticky lg:w-80 lg:shrink-0 lg:flex lg:flex-col lg:justify-end lg:items-end 2xl:max-w-lg 2xl:w-full dark:from-dark-800 dark:to-dark-700">
                 <div class="relative min-h-0 flex-1 flex flex-col xl:w-80">
                     <a href="/" class="flex items-center py-8 px-4 lg:px-8 xl:px-16">
                         <img
@@ -25,7 +25,7 @@
                             height="29"
                         >
                     </a>
-                    <div class="overflow-y-auto overflow-x-hidden px-4 lg:overflow-hidden lg:px-8 xl:px-16">
+                    <div class="px-4 pb-12 lg:px-8 xl:px-16">
                         <nav id="indexed-nav" class="hidden lg:block lg:mt-4">
                             <div class="docs_sidebar">
                                 {!! $index !!}
