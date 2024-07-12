33efd8156b697496f33aaf5abf3dca94367e3066
Tim MacDonald <hello@timacdonald.me>
2024-07-13 00:21:46 +1000

    Remove unused constants (#343)

--- routes/web.php
+++ routes/web.php
@@ -10,22 +10,6 @@ if (! defined('DEFAULT_VERSION')) {
     define('DEFAULT_VERSION', '11.x');
 }
 
-if (! defined('SHOW_VAPOR')) {
-    define('SHOW_VAPOR', random_int(1, 2) === 1);
-}
-
-if (! defined('SHOW_PROMO')) {
-    $int = random_int(1, 3);
-
-    if ($int === 1) {
-        define('SHOW_PROMO', 'FORGE');
-    } elseif ($int === 2) {
-        define('SHOW_PROMO', 'VAPOR');
-    } elseif ($int === 3) {
-        define('SHOW_PROMO', 'PARTNERS');
-    }
-}
-
 Route::get('docs', [DocsController::class, 'showRootPage']);
 
 Route::get('docs/cashier', function () {
