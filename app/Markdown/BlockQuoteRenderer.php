34f490a1bde8f91423bf842ce3355db79229f8fa
Tim MacDonald <hello@timacdonald.me>
2024-07-16 09:07:06 +1000

    Formatting

--- app/Markdown/BlockQuoteRenderer.php
+++ app/Markdown/BlockQuoteRenderer.php
@@ -14,14 +14,15 @@ class BlockQuoteRenderer implements NodeRendererInterface
 {
     /**
      * @return \Stringable|string|null
-     *
-     * @throws InvalidArgumentException if the wrong type of Node is provided
      */
     public function render(Node $node, ChildNodeRendererInterface $childRenderer)
     {
         $element = (new BaseBlockQuoteRenderer($node))->render($node, $childRenderer);
 
-        if (! $element instanceof HtmlElement || $element->getTagName() !== 'blockquote') {
+        if (
+            ! $element instanceof HtmlElement ||
+            $element->getTagName() !== 'blockquote'
+        ) {
             return $element;
         }
 
@@ -31,9 +32,9 @@ class BlockQuoteRenderer implements NodeRendererInterface
             return $element;
         }
 
-        $htmlPartial = Str::after($html, '<p>');
+        $paragraphPartial = Str::after($html, '<p>');
 
-        if (preg_match('/^\[!(NOTE|WARNING)\](?:<br \/>)?/', $htmlPartial, $matches) === 1) {
+        if (preg_match('/^\[!(NOTE|WARNING)\](?:<br \/>)?/', $paragraphPartial, $matches) === 1) {
             // GitHub styled notes, e.g.,
             // > [!NOTE] Content
             // or
@@ -44,8 +45,8 @@ class BlockQuoteRenderer implements NodeRendererInterface
                 'WARNING' => [Vite::content('resources/images/exclamation.svg'), 'bg-red-600'],
                 'NOTE' => [Vite::content('resources/images/lightbulb.svg'), 'bg-purple-600'],
             };
-        } elseif (preg_match('/^<strong>(Note|Warning):?<\/strong>:?(?:<br \/>)?/', $htmlPartial, $matches) === 1) {
-            // Legacy GitHub styled notes, e.g., 
+        } elseif (preg_match('/^<strong>(Note|Warning):?<\/strong>:?(?:<br \/>)?/', $paragraphPartial, $matches) === 1) {
+            // Legacy GitHub styled notes, e.g.,
             // > **Note:** Content
             // or
             // > **Note**: Content
@@ -65,7 +66,7 @@ class BlockQuoteRenderer implements NodeRendererInterface
                 'Warning' => [Vite::content('resources/images/exclamation.svg'), 'bg-red-600'],
                 'Note' => [Vite::content('resources/images/lightbulb.svg'), 'bg-purple-600'],
             };
-        } elseif (preg_match('/^\{(note|tip|video)\}/', $htmlPartial, $matches) === 1) {
+        } elseif (preg_match('/^\{(note|tip|video)\}/', $paragraphPartial, $matches) === 1) {
             // Legacy Laravel styled notes, e.g.,
             // > {tip} Content
 
@@ -78,14 +79,14 @@ class BlockQuoteRenderer implements NodeRendererInterface
             return $element;
         }
 
-        $bodyPartial = str_replace($matches[0], '', $htmlPartial);
+        $paragraphPartial = str_replace($matches[0], '', $paragraphPartial);
 
         return <<<HTML
             <div class="mb-10 max-w-2xl mx-auto px-4 py-8 shadow-lg lg:flex lg:items-center callout">
                 <div class="w-20 h-20 mb-6 flex items-center justify-center shrink-0 lg:mb-0 {$class}">
                     <div class="opacity-75">{$asset}</div>
                 </div>
-                <p class="mb-0 lg:ml-6 callout">{$bodyPartial}
+                <p class="mb-0 lg:ml-6 callout">{$paragraphPartial}
             </div>
             HTML;
     }
