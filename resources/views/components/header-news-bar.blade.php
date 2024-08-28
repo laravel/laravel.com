becc1f41cc8f18beeb69a121aafb172930bb1c38
Taylor Otwell <taylor@laravel.com>
2024-08-28 11:11:03 -0500

    wip

--- resources/views/components/header-news-bar.blade.php
+++ resources/views/components/header-news-bar.blade.php
@@ -1,5 +1,6 @@
 @php
     $news = ['nova', 'forge', 'vapor', 'herd', 'pulse', 'reverb', 'laracon-au'];
+    $news = ['cloud'];
 @endphp
 
 <div class="hidden lg:flex items-center justify-center bg-gradient-to-b from-red-500 to-red-600 p-2 text-center text-white text-sm h-9">
@@ -65,6 +66,14 @@
 
         <div class="mt-px ml-1">
             Deploy Laravel with the infinite scale of serverless using <a href="https://vapor.laravel.com" class="underline">Laravel Vapor</a>.
+        </div>
+    </template>
+
+    <template id="news-cloud">
+        {{-- <div><svg class="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z"></path></svg></div> --}}
+
+        <div class="mt-px ml-1">
+            Join the waitlist for <a href="https://cloud.laravel.com" class="underline">Laravel Cloud</a>, the future of shipping.
         </div>
     </template>
 
